d5d252751f347928696a602cf450a13ca3745aca
jaiswsho <34620018+jaiswsho@users.noreply.github.com>
2021-10-02 20:31:26 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,6 +13,10 @@ terraform {
 provider "azurerm" {
   skip_provider_registration = true
   features {}
+  client_id       = var.azurerm_client_id
+  client_secret   = var.azurerm_client_secret
+  subscription_id = var.azurerm_subscription_id
+  tenant_id       = var.azurerm_tenant_id
 }
 
 resource "azurerm_resource_group" "rg" {
