0b96b2a32b77be28acd65ae11f08710ec308a381
jaiswsho <34620018+jaiswsho@users.noreply.github.com>
2021-10-02 20:22:03 +1000

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,3 +1,21 @@
 variable "resource_group_name" {
   default = "myTFResourceGroup"
 }
+variable "azurerm_subscription_id" {
+  description = "The ID of the Subscription where the cluster will be deployed."
+  type        = string
+}
+
+variable "azurerm_tenant_id" {
+  description = "The ID of the AzureAD Tenancy"
+  type        = string
+}
+
+variable "azurerm_client_id" {
+  description = "The SPN Client ID used to deploy"
+  type        = string
+}
+
+variable "azurerm_client_secret" {
+  description = "The SPN Client Secret used to deploy"
+  type        = string
